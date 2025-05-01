5ad193c87ec01cf158c5e622251a1aeed2791325
DoNBaLooN <124310871+DoNBaLooN@users.noreply.github.com>
2025-05-01 15:31:17 +0300

    Update change-ssid.sh

--- change-ssid.sh
+++ change-ssid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Ask user for new SSID for 2.4 GHz Wi-Fi
+# Ask user for new SSID for 2.4 GHz Wi-Fi in green
 echo -ne "\033[32mEnter new SSID for 2.4 GHz Wi-Fi (default_radio0): \033[0m"
 read SSID_2G
 
@@ -11,13 +11,21 @@ SSID_5G="${SSID_2G}_5G"
 uci set wireless.default_radio0.ssid="$SSID_2G"
 uci set wireless.default_radio1.ssid="$SSID_5G"
 
+# Set the new description to match the 2.4 GHz SSID
+uci set system.@system[0].description="$SSID_2G"
+
 # Save the changes to the config
 uci commit wireless
+uci commit system
 
-# Reload Wi-Fi to apply changes
+# Reload Wi-Fi to apply changes first
 wifi reload
 
+# Reload system to apply description changes
+/etc/init.d/system reload
+
 # Show confirmation
-echo "SSIDs updated:"
+echo -e "\033[32mSSIDs and description updated:\033[0m"
 echo "2.4 GHz -> $SSID_2G"
 echo "5 GHz   -> $SSID_5G"
+echo "Description set to: $SSID_2G"
