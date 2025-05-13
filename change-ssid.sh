c356d79e403c1d46b4a841e950b80fed969bc745
DoNBaLooN <124310871+DoNBaLooN@users.noreply.github.com>
2025-05-13 14:51:52 +0300

    Update change-ssid.sh

--- change-ssid.sh
+++ change-ssid.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 
-# Ask user for new SSID for 2.4 GHz Wi-Fi in green
+# Запрос нового SSID для 2.4 ГГц Wi-Fi
 echo -ne "\033[32mEnter new SSID for 2.4 GHz Wi-Fi (default_radio0): \033[0m"
 read SSID_2G
 
-# Generate SSID for 5 GHz by appending _5G
+# Создание SSID для 5 ГГц
 SSID_5G="${SSID_2G}_5G"
 
-# Set the new SSIDs
+# Применение новых SSID
 uci set wireless.default_radio0.ssid="$SSID_2G"
 uci set wireless.default_radio1.ssid="$SSID_5G"
 
-# Set the new description to match the 2.4 GHz SSID
+# Обновление описания системы
 uci set system.@system[0].description="$SSID_2G"
 
-# Save the changes to the config
+# Генерация нового MAC-адреса (локально администрируемый)
+RANDMAC=$(hexdump -n5 -e '/1 ":%02X"' /dev/urandom)
+NEW_MAC="02${RANDMAC}"
+
+# Установка нового MAC-адреса для WAN
+uci set network.@device[1].macaddr="$NEW_MAC"
+
+# Сохранение изменений
 uci commit wireless
 uci commit system
+uci commit network
 
-# Reload Wi-Fi to apply changes first
+# Применение настроек
 wifi reload
-
-# Reload system to apply description changes
 /etc/init.d/system reload
+/etc/init.d/network restart
 
-# Show confirmation
+# Подтверждение
 echo -e "\033[32mSSIDs and description updated:\033[0m"
 echo "2.4 GHz -> $SSID_2G"
 echo "5 GHz   -> $SSID_5G"
 echo "Description set to: $SSID_2G"
+echo -e "\033[32mNew MAC address for WAN: $NEW_MAC\033[0m"
